d7fbe7fba0402cb7af71bd59998725c01b1de3f3
Chris Manson <chris@manson.ie>
2015-08-27 12:50:03 +0100

    removing some things

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,8 +1,6 @@
 import PouchDB from 'pouchdb';
 import { Adapter } from 'ember-pouch';
 
-// 5emPMCtDu7xGkEy7JsUcOjbYVk34X8
-// var remote = new PouchDB('https://mansona:5emPMCtDu7xGkEy7JsUcOjbYVk34X8@mansona.cloudant.com/leedsjs-leader-board');
 var remote = new PouchDB('http://atterceinapinesseledsher:ff54b8b62e1a10b56fb613419310c9b25e4b2f2a@leedsjs-leader-board.manson.ie');
 // var remote = new PouchDB('https://mansona.cloudant.com/leader-board');
 
